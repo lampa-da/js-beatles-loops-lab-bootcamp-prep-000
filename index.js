f5e09fb30baf7bdfbb962ec404e0a13efb8ef957
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-12-08 16:12:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musicians, instruments) {
   var array=[];
   for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[1] + instruments[1]);
-
   }
+  return array
 }
